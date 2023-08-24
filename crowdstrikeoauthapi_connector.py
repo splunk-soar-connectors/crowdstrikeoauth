3af9d53b6762dc7f48a965cbee4bd522b8be44af
aparekh-crest <aparekh@splunk.com>
2023-08-24 15:38:13 +0530

    Added scopes for all actions

--- crowdstrikeoauthapi_connector.py
+++ crowdstrikeoauthapi_connector.py
@@ -1726,8 +1726,6 @@ class CrowdstrikeConnector(BaseConnector):
             data.update({'assigned_to_uuid': assigned_to_user})
 
         if comment:
-            if len(bytes(comment, 'utf-8')) > MAXIMUM_COMMENT_LENGTH:
-                return action_result.set_status(phantom.APP_ERROR, CROWDSTRIKE_ERROR_INVALID_COMMENT_PARAM)
             data.update({'comment': comment})
 
         if status:

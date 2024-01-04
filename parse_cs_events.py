91d65ca3d09ba29b822365247b695aee548f1338
achandya <achandya@splunk.com>
2024-01-04 13:56:55 +0530

    Update failures

--- parse_cs_events.py
+++ parse_cs_events.py
@@ -1,6 +1,6 @@
 # File: parse_cs_events.py
 #
-# Copyright (c) 2019-2023 Splunk Inc.
+# Copyright (c) 2019-2024 Splunk Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- release_notes/unreleased.md
+++ release_notes/unreleased.md
@@ -1,1 +1,2 @@
 **Unreleased**
+* Add support for refreshing session token in 'on poll' to ingest data for more than 29minutes [PAPP-32493]

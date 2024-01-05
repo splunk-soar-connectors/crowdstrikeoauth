3f59a85c85c1e6a1461bc5b8fe19e09d86f2494a
jmeyer5 <jason_meyer@intuit.com>
2024-01-05 12:24:19 -0800

    added online status function

--- crowdstrikeoauthapi_connector.py
+++ crowdstrikeoauthapi_connector.py
@@ -718,6 +718,40 @@ class CrowdstrikeConnector(BaseConnector):
         self.debug_print('Successfully fetched device scroll with response {0}'.format(response))
         return action_result.set_status(phantom.APP_SUCCESS, "Device scroll fetched successfully")
 
+    def _handle_get_online_state(self, param):
+
+        # Add an action result to the App Run
+        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        fdid = param[CROWDSTRIKE_GET_ONLINE_STATE_DEVICE_ID]
+
+        api_data = {
+            "ids": fdid
+        }
+
+        ret_val, response = self._make_rest_call_helper_oauth2(action_result, CROWDSTRIKE_GET_ONLINE_STATE_ENDPOINT, params=api_data)
+
+        if phantom.is_fail(ret_val) and CROWDSTRIKE_STATUS_CODE_CHECK_MESSAGE in action_result.get_message():
+            return action_result.set_status(phantom.APP_SUCCESS, CROWDSTRIKE_NO_DATA_MESSAGE)
+
+        if phantom.is_fail(ret_val):
+            return action_result.get_status()
+
+        # successful request
+        try:
+            data = dict(response["resources"][0])
+        except Exception as ex:
+            return action_result.set_status(
+                phantom.APP_ERROR, "Error occurred while parsing response of 'get_online_state' action."
+                                   " Unknown response retrieved Error:{}".format(self._get_error_message_from_exception(ex)))
+
+        action_result.add_data(data)
+
+        summary = action_result.update_summary({})
+        self.debug_print('Successfully fetched device online state with response {0}'.format(response))
+        return action_result.set_status(phantom.APP_SUCCESS, "Device online state fetched successfully")
+
     def _handle_get_process_detail(self, param):
 
         # Add an action result to the App Run

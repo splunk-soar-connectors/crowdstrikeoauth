30007af88914645d7f3e97f45b29866521e35532
Scott Odle <sodle@splunk.com>
2026-07-20 09:50:49 -0600

    fix: confine report downloads to vault staging
    Reject report filename path components before writing downloaded bytes.\n\nTracks PSAAS-30563 (VULN-93288, FS-992).\n\nWritten by Codex.

--- release_notes/unreleased.md
+++ release_notes/unreleased.md
@@ -1,2 +1,3 @@
 **Unreleased**
 * Converted app to the SOAR SDK framework [PAPP-37876]
+* Restricted downloaded report filenames to the vault staging directory [PSAAS-30563]
---
+++ tests/test_download_report.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2026 Splunk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+from soar_sdk.exceptions import ActionFailure
+
+from src.actions.download_report import _validate_filename_stem
+
+
+@pytest.mark.parametrize(
+    "filename",
+    [
+        "../report",
+        "reports/report",
+        "/tmp/report",
+        "..\\report",
+        "reports\\report",
+        ".",
+        "..",
+        "report\0name",
+    ],
+)
+def test_validate_filename_stem_rejects_path_components(filename: str) -> None:
+    with pytest.raises(ActionFailure, match="must not contain path components"):
+        _validate_filename_stem(filename)
+
+
+def test_validate_filename_stem_accepts_plain_filename() -> None:
+    assert _validate_filename_stem("report-123") == "report-123"
